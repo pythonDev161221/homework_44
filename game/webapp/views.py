28d2fc4711caf8afaaca36e3b10ea69028275330
AISoftMaster <ormonkenenbayev@gmail.com>
2021-12-18 00:46:55 +0600

    mistakes catching improved on index.html and views.py to index.html

--- game/webapp/views.py
+++ game/webapp/views.py
@@ -4,15 +4,26 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+def num_list_create(N):
+    list_num = []
+    for i in range(N):
+        while True:
+            a = randint(1, 9)
+            if a not in secret_nums:
+                list_num.append(a)
+                break
+    return list_num
+
 def index_view(request):
-    global hid_num, context
+    global secret_nums, context, N
     if request.method == "GET":
-        hid_num = []
-        for i in range(4):
-            hid_num.append(randint(1, 10))
+        N = 4
+        secret_nums = []
+        secret_nums = num_list_create(N)
+
         context = {
             'answer': 'need to fill than there will be answer',
-            'hid_num': hid_num,
+            'secret_nums': secret_nums,
         }
         return render(request, 'index.html', context)
     elif request.method == 'POST':
@@ -26,9 +37,23 @@ def index_view(request):
         except ValueError:
             context['answer'] = 'it is need to input only numbers'
             return render(request, 'index.html', context)
+        n = num_list
+        if len(n) != N:
+            context['answer'] = f'it should be {N} numbers'
+            return render(request, 'index.html', context)
+        for i in n:
+            if i > 9 or i < 1:
+                context['answer'] = 'value should be between 1 and 9 included'
+                return render(request, 'index.html', context)
+            elif n.count(i) > 1:
+                context['answer'] = 'any number should not repeated twice or more'
+                return render(request, 'index.html', context)
+
+
+
 
         context = {
-            'answer': num_list,
-            'hid_num': hid_num,
+            'answer': n,
+            'secret_nums': secret_nums,
         }
     return render(request, "index.html", context)
