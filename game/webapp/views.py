df934a4092624018be0076e85bdedfceb062ff1e
AISoftMaster <ormonkenenbayev@gmail.com>
2021-12-17 22:20:58 +0600

    everything work but without calculation

--- game/webapp/views.py
+++ game/webapp/views.py
@@ -1,25 +1,37 @@
+from random import randint
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 # Create your views here.
 def index_view(request):
-    global context
+    global hid_num, context
     if request.method == "GET":
+        hid_num = []
+        for i in range(4):
+            hid_num.append(randint(1, 10))
         context = {
-            'answer': 'need to fill than there will be answer'
+            'answer': 'need to fill than there will be answer',
+            'hid_num': hid_num,
         }
         return render(request, 'index.html', context)
     elif request.method == 'POST':
-        context = {}
-        numbers = request.POST.get('num')
+
+        numbers = request.POST.get('num').strip()
+
         numbers = numbers.split(" ")
-        l = []
-        for i in numbers:
-            l.append(int(i))
-        # print(l)
+
+        num_list = []
+        try:
+            for i in numbers:
+                if i:
+                    num_list.append(int(i))
+        except:
+            context['answer'] = 'it is need to input only numbers'
+            return render(request, 'index.html', context)
 
         context = {
-            'answer': l
+            'answer': num_list,
+            'hid_num': hid_num,
         }
     return render(request, "index.html", context)
